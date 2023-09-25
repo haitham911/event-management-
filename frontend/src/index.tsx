9d62341d950c35ddd6691d326592aa62e4611f8e
haitham911 <haitham911eg@gmail.com>
2023-09-26 00:01:23 +0200

    front fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {HashRouter as Router, Route} from "react-router-dom";
-import {EventListContainer} from "./components/event_list_container";
-import {Navigation} from "./components/navigation";
-import {EventBookingFormContainer} from "./components/event_booking_form_container";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { EventListContainer } from "./components/event_list_container";
+import { Navigation } from "./components/navigation";
+import { EventBookingFormContainer } from "./components/event_booking_form_container";
 
 class App extends React.Component<{}, {}> {
     render() {
-        const eventList = () => <EventListContainer eventServiceURL="http://localhost:8181"/>;
-        const eventBooking = ({match}:any) => <EventBookingFormContainer eventID={match.params.id}
-                                                                         eventServiceURL="http://localhost:8181"
-                                                                         bookingServiceURL="http://localhost:8282"/>;
+        const eventList = () => <EventListContainer eventServiceURL="http://localhost:8181" />;
+        const eventBooking = ({ match }: any) => <EventBookingFormContainer eventID={match.params.id}
+            eventServiceURL="http://localhost:8181"
+            bookingServiceURL="http://localhost:8282" />;
 
         return <Router>
             <div>
-                <Navigation brandName="MyEvents"/>
+                <Navigation brandName="MyEvents" />
                 <div className="container">
                     <h1>My Events</h1>
-
-                    <Route exact path="/" component={eventList}/>
-                    <Route path="/events/:id/book" component={eventBooking}/>
+                    <Route  path="/" element={eventList} />
+                    <Route path="/events/:id/book" element={eventBooking} />
                 </div>
             </div>
         </Router>
@@ -27,6 +26,6 @@ class App extends React.Component<{}, {}> {
 }
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById("myevents-app")
 );
